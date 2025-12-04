e096f21a09591bb562806538272e5245c4081b42
praveen <bpraveen8040@gmail.com>
2025-12-04 10:57:43 +0530

    first commit

---
+++ src/main/java/com/praveen/docker_demo/DockerDemoController.java
@@ -0,0 +1,12 @@
+package com.praveen.docker_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerDemoController {
+    @GetMapping("/dockerDemo")
+    public String getDemo() {
+        return "Spring Boot App is Dockerized successfully";
+    }
+}
